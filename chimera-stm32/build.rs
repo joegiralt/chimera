071a504813d7196d4926a8e42745e0432423a8fa
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2026-04-21 10:06:09 +0200

    feat(stm32): add .ram_d2 linker section for audio buffers

--- chimera-stm32/build.rs
+++ chimera-stm32/build.rs
@@ -4,7 +4,26 @@ use std::path::PathBuf;
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     fs::copy("memory.x", out_dir.join("memory.x")).unwrap();
+
+    // Linker section for RAM_D2 audio buffers
+    fs::write(
+        out_dir.join("ram_d2.x"),
+        r#"
+SECTIONS {
+    .ram_d2 (NOLOAD) : ALIGN(4) {
+        *(.ram_d2 .ram_d2.*);
+        . = ALIGN(4);
+    } > RAM_D2
+}
+INSERT AFTER .bss;
+"#,
+    )
+    .unwrap();
+
     println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rustc-link-arg=-Tram_d2.x");
     println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=build.rs");
 }
